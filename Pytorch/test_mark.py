6fed256863e66d051f10e0d84687fe1173e8be1b
LuisC <luiscruzv99@gmial.com>
2021-03-15 12:11:18 +0100

    Modificada implementacion para que soporte entrenamineto distribuido
    El entrenamiento distribuido se realiza, de momento entre CPU y las GPUS disponibles, pero no se hace  equilibrio de carga entre los dispositivos (peor rendimiento de momento)

--- Pytorch/test_mark.py
+++ Pytorch/test_mark.py
@@ -1,15 +1,41 @@
+import os
+import sys
+import tempfile
+
 import Modelo.model as md
 import Modelo.training as tn
+
 import pickle as pk
-from torch.utils.data import TensorDataset, DataLoader
-import torch
 import numpy as np
 import time as tm
+
+import torch
+import torch.multiprocessing as mp
+import torch.distributed as dist
+
+from torch.utils.data import TensorDataset, DataLoader
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+# Anhadimos el cpu a la lista de dispositivos para poder enviarle trabajos
+devices = ['cpu']
+for i in range(0, torch.cuda.device_count()):
+    devices.append('cuda:'+str(i))
+
+
+def setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
+
+    dist.init_process_group("gloo", rank=rank, world_size=world_size)
+
+
+def cleanup():
+    dist.destroy_process_group()
 
 
 '''
 Funcion que detecta que dispositivo puede usar pytorch para entrenar la red
-'''
+
 def check_device():
 
     if torch.cuda.is_available():
@@ -18,6 +44,7 @@ def check_device():
         device = torch.device('cpu')  # Entrenar con el CPU
 
     return device
+'''
 
 '''
 Funcion que carga en arrays los datos de los archivos pickle
@@ -36,17 +63,25 @@ def load_data():
 Funcion que convierte de los arrays en tensores de pytorch, mandandolos a la GPU, definicion de datasets y dataloaders
 '''
 def convert_data(data, labels, device):
-    
+   
+    #Distribucion de cantidad de trabajos basado en dispositivo (no funciona)
+    end1 = 1000
+    end2 = 25
+    '''
+    if(device == 'cpu'):
+        end1 = 50
+    '''
+
     # Definicion de los tensores y dataloaders de la fase de entrenamiento
-    train_data = torch.Tensor(data[:4684]).to(device) 
-    train_labels = torch.Tensor(labels[:4684]).to(device) 
+    train_data = torch.Tensor(data[:end1]).to(device) 
+    train_labels = torch.Tensor(labels[:end1]).to(device) 
 
     train_set = TensorDataset(train_data,train_labels)
-    train_loader = DataLoader(train_set, batch_size=4, shuffle=True)
+    train_loader = DataLoader(train_set, batch_size=5, shuffle=True)
 
     # Definicion de los tensores y dataloaders de la fase de validacion 
-    val_data = torch.Tensor(data[30000:30750]).to(device)
-    val_labels = torch.Tensor(labels[30000:30750]).to(device)
+    val_data = torch.Tensor(data[end1:end1+end2]).to(device)
+    val_labels = torch.Tensor(labels[end1:end1+end2]).to(device)
 
     val_set = TensorDataset(val_data, val_labels)
     val_loader = DataLoader(val_set, shuffle=True)
@@ -55,30 +90,47 @@ def convert_data(data, labels, device):
 
 
 
-device = check_device()
-print("Usando dispositivo " + str(device))
+# device = check_device()
 
-st = tm.time()
-data, labels = load_data()
-print("Cargado de los datos: "+str(tm.time()-st))
+def loop(rank, world_size):
+    st = tm.time()
+    data, labels = load_data()
+    print("Cargado de los datos: "+str(tm.time()-st))
 
-st = tm.time()
-train_loader, val_loader = convert_data(data=data, labels=labels, device=device)
-print("Creacion de datasets: "+str(tm.time()-st))
 
-# Definicion de la red neuronal, optimizador y perdida
-net = md.resNet34(in_channels=1,n_classes=2).to(device)  # Instancia de ResNet34
-loss = torch.nn.MSELoss(reduction='mean').to(device)  # Funcion de perdida 
-optimizador = torch.optim.SGD(net.parameters(), lr=0.128)  # Optimizador de la red (Stochastic Gradient Deescent)
+    print("Usando dispositivo " + str(devices[rank]))
+    print(f"Trabajando en rango {rank}")
+    st = tm.time()
+    train_loader, val_loader = convert_data(data=data, labels=labels, device=devices[rank])
+    print("Creacion de datasets: "+str(tm.time()-st))
 
-# Fase de entrenamiento 
-st = tm.time()
-tn.train(train_loader, net, optimizador)
-print("Tiempo de entrenamiento: "+str(tm.time()-st))
+    # Definicion de la red neuronal, optimizador y perdida
+    setup(rank, world_size)
+    model = md.resNet34(in_channels=1,n_classes=2).to(devices[rank])  # Instancia de ResNet34
+    net = DDP(model) 
+    loss = torch.nn.MSELoss(reduction='mean') 
+    optimizador = torch.optim.SGD(net.parameters(), lr=0.128)  # Optimizador de la red (Stochastic Gradient Deescent)
+    
+    dist.barrier()
+    # Fase de entrenamiento 
+    st = tm.time()
+    tn.train(train_loader, net, optimizador)
+    print("Tiempo de entrenamiento: "+str(tm.time()-st))
+    
+    dist.barrier()
+    # Fase de validacion
+    st = tm.time()
+    acc, loss = tn.validate_or_test(val_loader, net, optimizador)
+    print("Tiempo de validacion: "+str(tm.time()-st))
+    print("Precision: "+str(acc))
+    print("Perdida: "+str(loss))
+    cleanup()
 
-# Fase de validacion
-st = tm.time()
-acc, loss = tn.validate_or_test(val_loader, net, optimizador)
-print("Tiempo de validacion: "+str(tm.time()-st))
-print("Precision: "+str(acc))
-print("Perdida: "+str(loss))
+def run_loop(loop_fn, world_size):
+    mp.spawn(loop_fn,
+            args=(world_size, ),
+            nprocs=world_size,
+            join=True)
+
+if __name__ == '__main__':
+    run_loop(loop, len(devices))
