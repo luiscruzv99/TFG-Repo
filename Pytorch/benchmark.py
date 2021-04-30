21c3406791d444bf4816fbefd8ecdff7c67436a6
Luis <luiscruzv99@gmail.com>
2021-04-30 12:12:20 +0200

    Terminada implementacion del script de benchmark
    Terminadas todas las funcionalidades que quedaban de implementar en el script de benchmark

--- Pytorch/benchmark.py
+++ Pytorch/benchmark.py
@@ -2,6 +2,8 @@ import os
 import sys
 from datetime import datetime
 
+import numpy
+
 import Modelo.model as md
 import Modelo.training as tn
 
@@ -24,13 +26,13 @@ for i in range(0, torch.cuda.device_count()):
 
 def inicializa_mundo(rank, world_size):
     """
-    Funcion que inicializa el grupo de procesos,
+    Funcion que inicializa el comunicador del grupo de procesos
 
     @type rank: int
     @param rank: rango del proceso actual
 
     @type world_size: int
-    @type world_size: numero de procesos en el grupo de procesos
+    @param world_size: numero de procesos en el grupo de procesos
     """
 
     os.environ['MASTER_ADDR'] = 'localhost'
@@ -49,7 +51,7 @@ def limpieza_mundo():
 
 def carga_datos():
     """
-    Funcion que carga en arrays los datos de los archivos binarios
+    Funcion que carga en listas los datos de los archivos binarios
     """
 
     with open('Entrenamiento/data.pkl', 'rb') as archivo_datos:
@@ -62,37 +64,44 @@ def carga_datos():
 
 def crea_tensores(parametros, datos, etiquetas, rango, tam_mundo):
     """
-    Funcion que convierte de los arrays en datasets y dataloaders
-    de Pytorch, el tamaño de estos depende del modo que se
-    este usando, y la cantidad de datos que se quieran mandar a
-    la red
+    Funcion que convierte los datos de las listas de muestras y etiquetas
+    en datasets y dataloaders de Pytorch, el tamaño de estos depende
+    del modo que se este usando, y la cantidad de datos que se quieran
+    mandar a la red.
 
-    @type parametros:
-    @param parametros:
+    @type parametros: dict
+    @param parametros: Diccionario que contiene los tamanhos de los diferentes conjuntos
 
-    @type datos:
-    @param datos:
+    @type datos: list
+    @param datos: Lista con todas las muestras a ensenhar a la RN
 
-    @type etiquetas:
-    @param etiquetas:
+    @type etiquetas: list
+    @param etiquetas: Lista con las etiquetas de las muestras (a que clase pertenecen)
 
-    @type rango:
-    @param rango:
+    @type rango: int
+    @param rango: Rango del proceso actual
 
-    @type tam_mundo:
-    @param tam_mundo:
+    @type tam_mundo: int
+    @param tam_mundo: Tamanho del grupo de procesos
     """
 
     tam_entren = int(parametros.get('Tam. entrenamiento') * len(datos))
     tam_val = int(parametros.get('Tam. validacion') * len(datos)) + tam_entren
 
+    # Ajuste del tamanho del conjunto de entrenamiento en caso de que se
+    # seleccione el modo de operacion que dsitribuye las muestras
+    # entre los dispositivos disponibles
     if parametros.get('Modo de operacion'):
-        tam_val = int((parametros.get('Tam. validacion') * len(datos) * tam_mundo) / rango) + tam_entren
-        tam_entren = int((parametros.get('Tam. entrenamiento') * len(datos) * tam_mundo) / rango)
+        tam_entren = tam_entren / tam_mundo
 
-    # Definicion de los tensores y dataloaders de la fase de entrenamiento
-    datos_entren = torch.Tensor(datos[:tam_entren])
-    etiquetas_entren = torch.Tensor(etiquetas[:tam_entren])
+        # Definicion de los tensores y dataloaders de la fase de entrenamiento
+        datos_entren = torch.Tensor(datos[tam_entren * rango:tam_entren * (rango + 1)])
+        etiquetas_entren = torch.Tensor(etiquetas[tam_entren * rango:tam_entren * (rango + 1)])
+
+    else:
+        # Definicion de los tensores y dataloaders de la fase de entrenamiento
+        datos_entren = torch.Tensor(datos[:tam_entren])
+        etiquetas_entren = torch.Tensor(etiquetas[:tam_entren])
 
     dataset_entren = TensorDataset(datos_entren, etiquetas_entren)
     loader_entren = DataLoader(dataset_entren, batch_size=parametros.get('Tam. batch'),
@@ -105,7 +114,7 @@ def crea_tensores(parametros, datos, etiquetas, rango, tam_mundo):
     dataset_val = TensorDataset(datos_val, etiquetas_val)
     loader_val = DataLoader(dataset_val, shuffle=True)
 
-    # Definicion de los tensores y dataloaders de la fase de test
+    # Definicion de los tensores y dataloaders de la fase de evaluacion
     datos_test = torch.Tensor(datos[tam_val:])
     etiquetas_test = torch.Tensor(etiquetas[tam_val:])
 
@@ -117,23 +126,23 @@ def crea_tensores(parametros, datos, etiquetas, rango, tam_mundo):
 
 def entrenamiento_resnet(rank, world_size, parametros, datos):
     """
-    Funcion que ejecuta en paralelo los bucles de entrenamiento y validacion de
-    la red neuronal, para ello recibe como parametros el rango del proceso
+    Funcion que ejecuta en paralelo los bucles de entrenamiento, validacion y evaluacion
+    de la red neuronal, para ello recibe como parametros el rango del proceso
     actual y el numero de procesos totales realizando la tarea.
     Los otros parametros recibidos se propagan a la funcion de creacion de los
     dataloaders con los que alimentar a la red neuronal.
 
-    @type rank:
-    @param rank:
+    @type rank: int
+    @param rank: rango del proceso actual
 
-    @type world_size:
-    @param world_size:
+    @type world_size: int
+    @param world_size: numero de procesos en el grupo de procesos
 
-    @type parametros:
-    @param parametros:
+    @type parametros: dict
+    @param parametros: Diccionario que contiene los tamanhos de los diferentes conjuntos
 
-    @type datos:
-    @param datos:
+    @type datos: list
+    @param datos: Lista con todas las muestras a ensenhar a la RN
     """
 
     # CONVERSION DE LOS DATOS AL FORMATO DE PYTORCH (TENSORES/DATALOADERS)
@@ -142,7 +151,7 @@ def entrenamiento_resnet(rank, world_size, parametros, datos):
     # DEFINICION DE LA RED NEURONAL, OPTIMIZADOR Y PERDIDA
 
     # Inicializacion del comunicador de procesos, previo al inicio del
-    # entrenamiento y validacion del modelo
+    # entrenamiento del modelo
     inicializa_mundo(rank, world_size)
 
     # Instancia de ResNet34 enviada al dispositivo designado al proceso
@@ -161,53 +170,50 @@ def entrenamiento_resnet(rank, world_size, parametros, datos):
     # Optimizador de la red (Stochastic Gradient Deescent)
     optimizador = torch.optim.SGD(modelo_paralelo.parameters(), lr=0.128)
 
-    # TODO: Implementar guardado de la precision para cada epoch
+    # ENTRENAMIENTO Y VALIDACION DE LA RED
+    precisiones_epochs = []
     st = tm.time()
-    for i in range(0, 24):
-        # Entrenamiento de la red
-        tn.train(dispositivos[rank], loader_entren, modelo_paralelo, optimizador)
-        # Validacion de la red
-        precision, perdida = tn.validate_or_test(dispositivos[rank], loader_val,
-                                         modelo_paralelo, optimizador)
+    for a in range(0, 24):
+        tn.train(dispositivos[rank], loader_entren, modelo_paralelo, optimizador, perdida_fn)
+        precision, = tn.validate_or_test(dispositivos[rank], loader_val, modelo_paralelo, optimizador, perdida_fn)
+        precisiones_epochs.append(precision)
 
     t_entren = tm.time() - st
 
-
-    # Test de la red
+    # EVALUACION DE LA RED
     st = tm.time()
-    precision, perdida = tn.validate_or_test(dispositivos[rank], loader_test,
-                                                modelo_paralelo, optimizador)
-    t_val = tm.time() - st
+    precision, perdida = tn.validate_or_test(dispositivos[rank], loader_test, modelo_paralelo, optimizador)
+    t_test = tm.time() - st
 
     # Eliminacion del grupo de procesos, operacion paralela terminada
     limpieza_mundo()
 
-    # Manera de retornar al hilo principal los resultados de la run, mediante
-    # la escritura de un archivo temporal, que el hilo principal lee al
+    # Manera de retornar al proceso principal los resultados de la ejecucion, mediante
+    # la escritura de un archivo temporal, que el proceso principal lee al
     # terminar la ejecucion de esta funcion
     if dispositivos[rank] == 'cuda:0':
-        with open('.' + dispositivos[rank], 'wb') as archivo_temp:
-            pk.dump([t_entren, t_val, precision, perdida], archivo_temp)
+        with open('.cuda:0', 'wb') as archivo_temp:
+            pk.dump([t_entren, t_test, precision, perdida, precisiones], archivo_temp)
 
 
 def paraleliza_funcion(funcion, num_dispositivos, parametros, datos):
     """
     Funcion que ejecuta una funcion, pasada como parametro, en paralelo. Para
-    ello spawnea tantos procesos como los especificados en el parametro
+    ello crea tantos procesos como los especificados en el parametro
     'world_size'.
     El resto de parametros se le pasan a la funcion paralelizada.
 
-    @type funcion:
-    @param funcion:
+    @type funcion: function
+    @param funcion: Funcion a ejecutar en paralelo
 
-    @type num_dispositivos:
-    @param num_dispositivos:
+    @type num_dispositivos: int
+    @param num_dispositivos: Numero de procesos a crear para ejecutar la funcion en paralelo
 
-    @type parametros:
-    @param parametros:
+    @type parametros: dict
+    @param parametros: Diccionario que contiene los tamanhos de los diferentes conjuntos
 
-    @type datos:
-    @param datos:
+    @type datos: list
+    @param datos: Lista con todas las muestras a ensenhar a la RN
     """
     mp.spawn(funcion,
              args=(num_dispositivos, parametros, datos),
@@ -216,16 +222,17 @@ def paraleliza_funcion(funcion, num_dispositivos, parametros, datos):
 
 
 if __name__ == '__main__':
-    '''
+    """
     Funcion principal de la script. Recibe como parametros (de la linea de
-    comandos) el tamanho de entrenamiento y validacion de los conjuntos de
-    datos,tamanho de los batches del conjunto de entrenamiento y la proporcion
-    en la que reducir el tamanho de los datos de entrenamiento para la CPU
-    (equilibrado de carga).
+    comandos) el tamanho  de los conjuntos de de entrenamiento, validacion,
+    tamanho de los batches del conjunto de entrenamiento y como distribuir
+    los datos entre los dispositivos (0 = todos los dispositivos ven los mismos datos,
+    1 = cada dispositivo ve una fraccion de los datos totales).
+    
     Estos parametros deben introducirse en el orden especificado anteriormente,
     en caso de faltar alguno, se utilizaran valores por defecto para los
     parametros no intorducidos.
-    '''
+    """
 
     # Parametros por defecto
     tamanho_entren = 0.7
@@ -240,22 +247,24 @@ if __name__ == '__main__':
         tamanho_test = sys.argv[3]
         tamanho_batch = sys.argv[4]
         dividir_conjuntos = sys.argv[5]
+
     except IndexError:
         print("No se han introducido todos los parametros esperados," +
               " usando valores por defecto")
 
     # Diccionario con los parametros con los que se ha ejecutado el benchamrk
     params = {'Tam. entrenamiento': tamanho_entren, 'Tam. validacion': tamanho_val,
-              'Tam. batch': tamanho_batch,
-              'Modo de operacion': dividir_conjuntos, 'Dispositivos': dispositivos}
+              'Tam. batch': tamanho_batch, 'Modo de operacion': dividir_conjuntos,
+              'Dispositivos': dispositivos}
 
+    # Lectura de las muestras y etiquetas desde sus respectivos archivos
     lista_datos = carga_datos()
 
     # Lista con los resultados obtenidos
     resultados = []
 
-    # Bucle del benchmark, que ejecuta el entrenamiento y validacion de la red
-    # neuronal, distribuyendolo entre los dispositivos disponibles (CPU y GPUs)
+    # Bucle del benchmark, que ejecuta el entrenamiento y evaluacion de la red
+    # neuronal, distribuyendolo entre los dispositivos disponibles (GPUs) y
     # cogiendo los resultados que devuelve el dispositivo 'cuda:0' (los resul-
     # tados son iguales para todos los dispositivos)
     for i in range(0, 10):
@@ -270,9 +279,18 @@ if __name__ == '__main__':
     # t. val...) y cada columna es una run
     os.remove('.cuda:0')
 
+    # Guardado de la evolucion de las precisiones a lo largo de los epochs
+    precisiones = []
+    for run in resultados:
+        precisiones.append(run.pop())
+
     resultados = np.transpose(np.array(resultados))
 
     # Guardado de los resultados en un archivo, con la fecha y hora en la que
     # se termino el benchamrk
     with open(datetime.now().strftime('%Y-%m-%d,%H:%M:%S') + '.bbr', 'wb') as f:
         pk.dump([params, resultados], f)
+
+    # Guardado de las precisiones de cada run
+    numpy.savetxt('Precisiones ' + datetime.now().strftime('%Y-%m-%d,%H:%M:%S') + '.csv',
+                  np.array(precisiones), delimiter=',')
